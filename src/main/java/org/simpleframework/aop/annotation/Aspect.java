e28bbf3e54fabae066d222f6600b907a25ce09cf
aameiwei <>
2022-09-18 21:37:52 +0800

    20220918 spring自研架构项目总体

---
+++ src/main/java/org/simpleframework/aop/annotation/Aspect.java
@@ -0,0 +1,14 @@
+package org.simpleframework.aop.annotation;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Aspect {
+    String pointcut();
+    //"execution(* com.imooc.controller.frontend..*.*(..))"以及within(com.imooc.controller.frontend.*)
+    /**
+     * 版本一：需要被织入横切逻辑的注解标签
+     */
+    //Class<? extends Annotation> value();
+}
